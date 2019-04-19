737e90d0097cf39291880f062c490cb7f50ceb98
Lucy Barber <lbarber729@hotmail.co.uk>
2019-04-19 12:41:24 +0100

    fizzbuzz 2

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,9 @@
 def fizzbuzz(number)
-  "Fizz"
+  if number % 3 == 0
+    "Fizz"
+  elsif number == 5
+    "Buzz"
+  else
+    number
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,6 +2,28 @@ require 'fizzbuzz'
 
 describe "fizzbuzz" do
   it "returns 'fizz' when passed 3" do
-    expect (fizzbuzz(3)).to eq 'Fizz'
+    expect(fizzbuzz(3)).to eq "Fizz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns the number if it doesn't transform" do
+    expect(fizzbuzz(4)).to eq 4
+  end
+end
+
+describe "fizzbuzz for multiples of 3" do
+  it "returns 'fizz' when passed a multiple of 3" do
+    expect(fizzbuzz(6)).to eq "Fizz"
+    expect(fizzbuzz(9)).to eq "Fizz"
+    expect(fizzbuzz(12)).to eq "Fizz"
+    expect(fizzbuzz(81)).to eq "Fizz"
+    expect(fizzbuzz(300)).to eq "Fizz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns 'buzz' when passed 5" do
+    expect(fizzbuzz(5)).to eq "Buzz"
   end
 end
