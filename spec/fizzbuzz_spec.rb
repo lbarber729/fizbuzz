487cf00243f3c9587b57cce4450d9c0bbf61dbc6
Lucy Barber <lbarber729@hotmail.co.uk>
2019-04-19 12:50:36 +0100

    fizzbuzz 2

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -18,7 +18,7 @@ describe "fizzbuzz for multiples of 3" do
     expect(fizzbuzz(9)).to eq "Fizz"
     expect(fizzbuzz(12)).to eq "Fizz"
     expect(fizzbuzz(81)).to eq "Fizz"
-    expect(fizzbuzz(300)).to eq "Fizz"
+    expect(fizzbuzz(303)).to eq "Fizz"
   end
 end
 
@@ -27,3 +27,21 @@ describe "fizzbuzz" do
     expect(fizzbuzz(5)).to eq "Buzz"
   end
 end
+
+describe "fizzbuzz for multiples of 5" do
+  it "returns 'buzz' when passed a multiple of 5" do
+    expect(fizzbuzz(10)).to eq "Buzz"
+    expect(fizzbuzz(25)).to eq "Buzz"
+    expect(fizzbuzz(40)).to eq "Buzz"
+    expect(fizzbuzz(100)).to eq "Buzz"
+    expect(fizzbuzz(145)).to eq "Buzz"
+  end
+end
+
+describe "fizzbuzz for multiples of 3 and 5" do
+  it "returns 'fizzbuzz' when passed a multiple of 3 and 5" do
+    expect(fizzbuzz(15)).to eq "FizzBuzz"
+    expect(fizzbuzz(30)).to eq "FizzBuzz"
+    expect(fizzbuzz(45)).to eq "FizzBuzz"
+  end
+  end
